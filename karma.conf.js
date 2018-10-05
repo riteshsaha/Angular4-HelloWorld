4fd9aef82574ea49e4e2b07c71980231c670f2cf
riteshsaha <42380085+riteshsaha@users.noreply.github.com>
2018-10-05 12:14:53 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,23 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['HeadlessChromium'],
+    customLaunchers: {
+      HeadlessChromium: {
+        base: 'ChromiumHeadless',
+        flags: [
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+          '--enable-logging',
+          '--user-data-dir=./karma-chrome',
+          '--v=1',
+          '--disable-background-timer-throttling',
+          '--disable-renderer-backgrounding',
+          '--proxy-bypass-list=*',
+          '--proxy-server=\'direct://\''
+       ]
+      }
+    }
     
     singleRun: false,
     coverageIstanbulReporter: {
