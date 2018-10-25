bdfc0310c8cb1eb1bd9ce30b1c69f4d57482b7ac
riteshsaha <42380085+riteshsaha@users.noreply.github.com>
2018-10-25 11:28:44 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
-const path = require('path');
+//const path = require('path');
 const process = require('process');
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
